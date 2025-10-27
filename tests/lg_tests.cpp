4e2d66ddfbb56049e160e989b6edbecf7293b90c
Fyrmon Warslay <fyrmonwarslay@gmail.com>
2025-10-27 22:21:47 +0100

    Add binary NOR and multi NOR and its tests

--- tests/lg_tests.cpp
+++ tests/lg_tests.cpp
@@ -3,42 +3,56 @@
 
 TEST(LG_Test, LG_OR)
 {
-    EXPECT_EQ(LogicGates::OR(1,1),true);
-    EXPECT_EQ(LogicGates::OR(0,1),true);
-    EXPECT_EQ(LogicGates::OR(0,0),false );
+    EXPECT_TRUE(LogicGates::OR(1,1));
+    EXPECT_TRUE(LogicGates::OR(0,1));
+    EXPECT_FALSE(LogicGates::OR(0,0));
 
-    EXPECT_EQ(LogicGates::OR({0,0,0,0,0,1}),true);
-    EXPECT_EQ(LogicGates::OR({0,0,0,0}),false);
+    EXPECT_TRUE(LogicGates::OR({0,0,0,0,0,1}));
+    EXPECT_FALSE(LogicGates::OR({0,0,0,0}));
 }
 
 TEST(LG_Test, LG_AND)
 {
-    EXPECT_EQ(LogicGates::AND(true,true),true);
-    EXPECT_EQ(LogicGates::AND(true,false),false);
-    EXPECT_EQ(LogicGates::AND(false,false),false);
+    EXPECT_TRUE(LogicGates::AND(true,true));
+    EXPECT_FALSE(LogicGates::AND(true,false));
+    EXPECT_FALSE(LogicGates::AND(false,false));
 
-    EXPECT_EQ(LogicGates::AND({1,1,1,1,1}),true);
-    EXPECT_EQ(LogicGates::AND({1,1,1,1,0}),false);
+    EXPECT_TRUE(LogicGates::AND({1,1,1,1,1}));
+    EXPECT_FALSE(LogicGates::AND({1,1,1,1,0}));
 }
 
 TEST(LG_TEST, LG_NOT)
 {
-    EXPECT_EQ(LogicGates::NOT(true),false);
-    EXPECT_EQ(LogicGates::NOT(false),true);
+    EXPECT_FALSE(LogicGates::NOT(true));
+    EXPECT_TRUE(LogicGates::NOT(false));
 }
 
 TEST(LG_TEST, LG_XOR)
 {
-    EXPECT_EQ(LogicGates::XOR(0,1),true);
-    EXPECT_EQ(LogicGates::XOR(1,0),true);
-    EXPECT_EQ(LogicGates::XOR(1,1),false);
-    EXPECT_EQ(LogicGates::XOR(0,0),false);
+    EXPECT_TRUE(LogicGates::XOR(0,1));
+    EXPECT_TRUE(LogicGates::XOR(1,0));
+    EXPECT_FALSE(LogicGates::XOR(1,1));
+    EXPECT_FALSE(LogicGates::XOR(0,0));
 
-    EXPECT_EQ(LogicGates::XOR({1,1,0,0}),false);
-    EXPECT_EQ(LogicGates::XOR({1,0,0,0,0}),true);
-    EXPECT_EQ(LogicGates::XOR({1,0,0,0,0,1}),false);
-    EXPECT_EQ(LogicGates::XOR({1,0,0,0,1,0,0,1}),true);
-    EXPECT_EQ(LogicGates::XOR({1,0,1,0,1,0,1,1}),true);
-    EXPECT_EQ(LogicGates::XOR({1,0}),true);
-    EXPECT_EQ(LogicGates::XOR({0}), false);
+    EXPECT_FALSE(LogicGates::XOR({1,1,0,0}));
+    EXPECT_TRUE(LogicGates::XOR({1,0,0,0,0}));
+    EXPECT_FALSE(LogicGates::XOR({1,0,0,0,0,1}));
+    EXPECT_TRUE(LogicGates::XOR({1,0,0,0,1,0,0,1}));
+    EXPECT_TRUE(LogicGates::XOR({1,0,1,0,1,0,1,1}));
+    EXPECT_TRUE(LogicGates::XOR({1,0}));
+    EXPECT_FALSE(LogicGates::XOR({0}));
+}
+
+TEST(LG_TEST, LG_NOR)
+{
+    EXPECT_TRUE(LogicGates::NOR(0,0));
+    EXPECT_FALSE(LogicGates::NOR(1,0));
+    EXPECT_FALSE(LogicGates::NOR(0,1));
+    EXPECT_FALSE(LogicGates::NOR(1,1));
+
+    EXPECT_TRUE(LogicGates::NOR({0,0,0,0,0,0}));
+    EXPECT_FALSE(LogicGates::NOR({0,0,0,0,0,1}));
+    EXPECT_FALSE(LogicGates::NOR({0,1,0,0,0,0}));
+    EXPECT_TRUE(LogicGates::NOR({0,0}));
+    EXPECT_FALSE(LogicGates::NOR({0,1}));
 }
